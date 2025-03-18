304eb47e0b0e30e372af51f00b5a0a6491194f82
tingyinhu <yin5683@gmail.com>
2025-03-18 16:34:15 -0700

    Use layout and outlet to make it clean 🧹

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Sidebar from "./Components/global/Sidebar";
 import Footer from "./Components/global/Footer";
 import HomePage from "./Pages/HomePage";
@@ -16,46 +16,60 @@ import UiuProject from "./Pages/ProjectPages/UiuProject";
 import PetronewProject from "./Pages/ProjectPages/PetronewProject";
 import CanadacaProject from "./Pages/ProjectPages/CanadacaProject";
 
+const Layout = () => {
+  return (
+    <div className="relative grid grid-cols-4 gap-4 md:grid-cols-12 md:gap-5 min-h-screen">
+      {/* Empty column for spacing on the left (1 column) */}
+      <div className="hidden md:block md:col-span-1"></div>
+
+      {/* Sidebar: 1 col desktop, hidden on mobile */}
+      <div className="md:col-span-1">
+        <Sidebar />
+      </div>
+
+      {/* Main Content - 4 cols mobile, 9 cols desktop */}
+      <div className="col-span-4 md:col-span-9 m-4">
+        <Outlet />
+      </div>
+
+      {/* Empty column for spacing on the right (1 column) */}
+      <div className="hidden md:block md:col-span-1"></div>
+
+      {/* Footer - full width */}
+      <div className="col-span-4 md:col-span-12">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      {/* Grid: 4 cols mobile, 12 cols desktop */}
-      {/* relative: project pages baneer */}
-      <div className="relative grid grid-cols-4 gap-4 md:grid-cols-12 md:gap-5 ">
-        {/* Sidebar: 1 col desktop, hidden mobile */}
-        <div className="md:col-span-1">
-          <Sidebar />
-        </div>
-        <div className="md:col-span-1"></div>
-        {/* Main Content - 4 cols 16px margin mobile, 11 columns on laptop/desktop, margin 1 column in each */}
-        <div className="col-span-4 m-4 md:col-span-9">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-            <Route path="/Casscycle-Project" element={<CasscycleProject />} />
-            <Route
-              path="/Mobin-Donut-Project"
-              element={<MobinDonutProject />}
-            />
-            <Route path="/Eatery-Project" element={<EateryProject />} />
-            <Route path="/Kavi-Project" element={<KaviProject />} />
-            <Route path="/Lms-Project" element={<LmsProject />} />
-            <Route path="/Tiannli-Project" element={<TiannliProject />} />
-            <Route
-              path="/ToliCuliPali-Project"
-              element={<ToliCuliPaliProject />}
-            />
-            <Route path="/Viida-Project" element={<ViidaProject />} />
-            <Route path="/Uiu-Project" element={<UiuProject />} />
-            <Route path="/Petronew-Project" element={<PetronewProject />} />
-            <Route path="/Canadaca-Project" element={<CanadacaProject />} />
-          </Routes>
-        </div>
-
-        <div className="md:col-span-1"></div>
-      </div>
-      <Footer />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/Casscycle-Project" element={<CasscycleProject />} />
+          <Route
+            path="/Mobin-Donut-Project"
+            element={<MobinDonutProject />}
+          />
+          <Route path="/Eatery-Project" element={<EateryProject />} />
+          <Route path="/Kavi-Project" element={<KaviProject />} />
+          <Route path="/Lms-Project" element={<LmsProject />} />
+          <Route path="/Tiannli-Project" element={<TiannliProject />} />
+          <Route
+            path="/ToliCuliPali-Project"
+            element={<ToliCuliPaliProject />}
+          />
+          <Route path="/Viida-Project" element={<ViidaProject />} />
+          <Route path="/Uiu-Project" element={<UiuProject />} />
+          <Route path="/Petronew-Project" element={<PetronewProject />} />
+          <Route path="/Canadaca-Project" element={<CanadacaProject />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
